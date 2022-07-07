f975436ef656f09d3d5bdcc82b9108adc3cb8cce
Halimeh Habib Diñeiro <halimehhabibdineiro@MacBook-Pro-de-Halimeh.local>
2022-07-07 13:00:43 +0200

    Nivel 3 completado, ejercicio 7 empezado

--- src/films.js
+++ src/films.js
@@ -17,16 +17,16 @@ function getMoviesFromDirector(array, director) {
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  const moviesFromDirector = array.filter(item => item.director.includes(director));
-  const scoreFromTheMovies = moviesFromDirector.map(item => item.score)
-  const sumScores = scoreFromTheMovies.reduce(
-    (prev, curr) => prev + curr, 0
+  const moviesFromDirector = array.filter((item) =>
+    item.director.includes(director)
   );
+  const scoreFromTheMovies = moviesFromDirector.map((item) => item.score);
+  const sumScores = scoreFromTheMovies.reduce((prev, curr) => prev + curr, 0);
   // console.log('jose', sumScores)
   // console.log('pepe', scoreFromTheMovies)
-  const averageScore = sumScores / moviesFromDirector.length
+  const averageScore = sumScores / moviesFromDirector.length;
   console.log('EXERCISE 3 ->', averageScore);
-  return averageScore
+  return averageScore;
 }
 
 // Exercise 4:  Alphabetic order by title
@@ -47,6 +47,7 @@ function orderByYear(array) {
       if (a.title < b.title) return -1;
       if (a.title === b.title) return 0;
     });
+// el sort se usa con if porque son strings y no números, con números se puede escribir en corto
   orderedArray.sort((a, b) => a.year - b.year);
 
   console.log('EXERCISE 5 ->', orderedArray);
@@ -55,30 +56,45 @@ function orderByYear(array) {
 
 // Exercise 6: Calculate the average of the movies in a category
 function moviesAverageByCategory(array, genre) {
-// ignorar las pelis sin score  
-const moviesScores = array.filter(item => item.score !== '')
-const moviesCategory = moviesScores.filter(item => item.genre.includes(genre))
-const moviesCategoryAndScore = moviesCategory.map(item => item.score)
-const sumValues = moviesCategoryAndScore.reduce((a, b) => a + b, 0)
-const average = sumValues / moviesCategoryAndScore.length
-const rounded = Math.round(average * 100) / 100
-console.log('EXERCISE 6 ->', average)
-return rounded
+  // ignorar las pelis sin score
+  const moviesScores = array.filter((item) => item.score !== '');
+  const moviesCategory = moviesScores.filter((item) =>
+    item.genre.includes(genre)
+  );
+  const moviesCategoryAndScore = moviesCategory.map((item) => item.score);
+  const sumValues = moviesCategoryAndScore.reduce((a, b) => a + b, 0);
+  const average = sumValues / moviesCategoryAndScore.length;
+  const rounded = Math.round(average * 100) / 100;
+  console.log('EXERCISE 6 ->', average);
+  return rounded;
 }
 
 // Exercise 7: Modify the duration of movies to minutes
 function hoursToMinutes(array) {
+const moviesDuration = array.map(item => item.duration)
+const hours = moviesDuration.splice(0, 1)
+const minutesMovies = moviesDuration.splice(3, 5)
+const hoursToMinutes = parseInt(hours)
+let minutes = 0
+if (hoursToMinutes >= 1) {
+ minutes += hoursToMinutes * 60 + minutesMovies
+} 
+  
+  console.log('maruja', minutes)
+  console.log('pepe', hours)
+  console.log('lola', hoursToMinutes)
+  return moviesDuration
 }
 
 // Exercise 8: Get the best film of a year
 function bestFilmOfYear(array) {
-  const moviesYear = array.filter(item => item.year)
-  const moviesScores = moviesYear.sort((a, b) => b.score - a.score)
-  const bestOfTheYear = moviesScores.slice(0, 1)
-  console.log('lola', moviesScores)
-  console.log('EXERCISE 8 ->', bestOfTheYear)
- 
-  return bestOfTheYear
+  const moviesYear = array.filter((item) => item.year);
+  const moviesScores = moviesYear.sort((a, b) => b.score - a.score);
+  const bestOfTheYear = moviesScores.slice(0, 1);
+  //console.log('lola', moviesScores);
+  console.log('EXERCISE 8 ->', bestOfTheYear);
+
+  return bestOfTheYear;
 }
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
